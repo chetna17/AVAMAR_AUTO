076cd1c6387502badda54fe9d04d9d729b2a91e3
chetna17 <chetna.iet@gmail.com>
2018-09-10 12:30:53 +0100

    change command

--- AvamarGrid.py
+++ AvamarGrid.py
@@ -1,7 +1,7 @@
 import subprocess
 def showgroup():
     mccli = "mccli group show --mcsprofile=ix20"
-    //p = subprocess.check_output(mccli,shell=True)
+    #p = subprocess.check_output(mccli,shell=True)
     x = subprocess.call(mccli,shell=True)
     print(x)
 
